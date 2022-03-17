927b5520a1b9d0830ed8b1e6ceb435136d9d0e71
kevnviek303@yahoo.com <kevnivek303@yahoo.com>
2022-03-17 16:02:57 -0400

    updated js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require("fs");
+const path = require("path");
+
 // TODO: Create an array of questions for user input
 const questions = [];
 
@@ -16,12 +20,24 @@ function init() {
         name: "title",
       },
       {
+        type: "list",
+        message: "What license will you be using for this project?",
+        name: "license",
+        choices: ["MIT", "IBM", "Unlicense", "Perl", "Mozilla"],
+      },
+      {
         type: "input",
         message: "What is the description of your project?",
         name: "description",
       },
     ])
-    .then((answers) => console.log(answers));
+    .then((answers) => {
+      console.log(generateMarkdown(answers));
+      fs.writeFileSync(
+        path.join(process.cwd(), "newReadMe.md"),
+        generateMarkdown(answers)
+      );
+    });
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,24 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "MIT":
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    case "IBM":
+      return "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)";
+
+    case "Perl":
+      return "[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)";
+
+    case "Unlicense":
+      return "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)";
+    case "Mozilla":
+      return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+
+    default:
+      return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -13,6 +31,11 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
+  ${renderLicenseBadge(data.license)}
+
+  ## Description
+  ${data.description}
 
 `;
 }
